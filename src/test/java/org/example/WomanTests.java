d20d66911afeea3d738fc99dacd343afc69b9111
Irishka <irina.teremenko@gmail.com>
2023-05-28 12:16:28 +0300

    Second commit

--- src/test/java/org/example/WomanTests.java
+++ src/test/java/org/example/WomanTests.java
@@ -1,45 +1,44 @@
 package org.example;
 
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WomanTests {
 
-    @Test
-    public void testIsRetired() {
-        Woman woman = new Woman("Alice", "Johnson", 65);
-        Assert.assertTrue(woman.isRetired());
-
-        Woman woman2 = new Woman("Sarah", "Brown", 50);
-        Assert.assertFalse(woman2.isRetired());
-    }
-
-    @Test
+    @Test(groups = "getters")
     public void testGetFirstName() {
         Woman woman = new Woman("Alice", "Johnson", 65);
-        Assert.assertEquals(woman.getFirstName(), "Alice");
+        Assert.assertEquals(woman.getFirstName(), "Alice", "Incorrect first name");
     }
 
-    @Test
+    @Test(groups = "getters")
     public void testGetLastName() {
         Woman woman = new Woman("Alice", "Johnson", 65);
-        Assert.assertEquals(woman.getLastName(), "Johnson");
+        Assert.assertEquals(woman.getLastName(), "Johnson", "Incorrect last name");
     }
 
-    @Test
+    @Test(groups = "getters")
     public void testGetAge() {
         Woman woman = new Woman("Alice", "Johnson", 65);
-        Assert.assertEquals(woman.getAge(), 65);
+        Assert.assertEquals(woman.getAge(), 65, "Incorrect age");
     }
 
-    @Test
+    @Test(groups = "getPartner")
     public void testGetPartnerLastName() {
         Woman woman = new Woman("Alice", "Johnson", 65);
         Man partner = new Man("Bob", "Smith", 70);
         woman.setPartner(partner);
 
-        Assert.assertEquals(woman.getLastName(),
-                "Smith");
+        Assert.assertEquals(woman.getLastName(), "Smith", "Incorrect partner last name");
     }
 
+    @Test(groups = "isRetired")
+    public void testIsRetired() {
+        Woman woman = new Woman("Alice", "Johnson", 65);
+        Assert.assertTrue(woman.isRetired(), "Expected retired");
+
+        Woman woman2 = new Woman("Sarah", "Brown", 50);
+        Assert.assertFalse(woman2.isRetired(), "Expected not retired");
+    }
 }
